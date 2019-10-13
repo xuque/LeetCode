454bc518f50735cc1ba9ca5e17b892f39a37e919
xuque <xueque0613@gmail.com>
2019-10-13 22:34:08 +0800

    optimize title 3

--- 3.Longest Substring Without Repeating Characters/Longest Substring Without Repeating Characters.cpp
+++ 3.Longest Substring Without Repeating Characters/Longest Substring Without Repeating Characters.cpp
@@ -6,24 +6,22 @@ public:
         }
         int front = 0;
         int rear = 1;
-        int len = 1;
         int record_len = 1;
         std::map<char, bool>record;
         record.insert(std::pair<char,bool>(s[0],true));
         while(rear < s.size()){
             if(record.find(s[rear]) != record.end()){
+                while(s[front] != s[rear]){
+                    record.erase(s[front++]);
+                }
                 ++front;
-                record.clear();
-                rear = front;
-                len = 1;
             }
             else{
-                ++len;
-                if(record_len < len){
-                    record_len = len;
+                record.insert(std::pair<char,bool>(s[rear],true));
+                if(record_len < record.size()){
+                    record_len = record.size();
                 }
             }
-            record.insert(std::pair<char,bool>(s[rear],true));
             ++rear;
         }
         return record_len;
